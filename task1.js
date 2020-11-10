4d3684c119f73934b89b039cca504661755c86e3
TheMaja <majawonke@gmail.com>
2020-11-10 06:39:46 +0200

    BODMAS

--- task1.js
+++ task1.js
@@ -1,15 +1,8 @@
-/*
-x = 0
-y = 1
-Print the value of x
-Print the value of y
-x = x + 3
-y = y + x
-Print the value of x
-Print the value of y
-*/
-
-let x = 0;
-let y = 1;
+x = 0;
+y = 1;
+console.log(x)
+console.log(y)
+x += 3;
+y += x;
 console.log(x)
 console.log(y)
