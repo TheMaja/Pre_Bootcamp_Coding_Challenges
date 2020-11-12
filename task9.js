bbfd8087d89d59f894ba370a4a2a423d1ff76c02
TheMaja <majawonke@gmail.com>
2020-11-12 16:53:19 +0200

    task 9

---
+++ task9.js
@@ -0,0 +1,14 @@
+function naturalnum(num) {
+    var count = 0;
+    for(let i = 0; i < num ; i++)
+    {
+	if(i % 3  == 0)
+	    count += i
+	if(i % 5 == 0)
+	    count += i
+    }
+    return count;
+}
+
+//naturalnum(1000) => 266333
+The sum of all the multiples of 3 or 5 below 1000 is 266333.
