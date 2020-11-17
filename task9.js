d4cfc9cbb7b298a8241b46e5d74d1dd47a32dee4
TheMaja <majawonke@gmail.com>
2020-11-17 08:20:46 +0200

    function name fix

--- task9.js
+++ task9.js
@@ -2,7 +2,7 @@
 This function returns the sum of all the multiples of 3 or 5 below a certain input number.
 */
 
-function naturalnum(num) {
+function naturalNumber(num) {
     var count = 0;
     for(let i = 0; i < num ; i++)
     {
